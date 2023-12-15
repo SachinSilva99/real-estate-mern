ca0a04ba9761396ca71090d193d621156f1cf320
SachinSilva99 <sachinmanathunga@gmail.com>
2023-12-15 21:36:15 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-
-const About = () => {
-  return <div>About</div>;
-};
-
-export default About;
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Real Estate</h1>
+      <p className="mb-4 text-slate-700">
+        Explore the world of Realty Realm, a premier real estate agency focused
+        on assisting clients with buying, selling, and renting properties in
+        highly sought-after neighborhoods. Our team of seasoned agents is
+        committed to delivering exceptional service, ensuring a seamless
+        experience throughout the buying and selling journey.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission is to guide clients in achieving their real estate
+        aspirations by offering expert advice, personalized service, and an
+        in-depth understanding of the local market. Whether you're in the market
+        to buy, sell, or rent, we're here to support you at every stage.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Backed by a wealth of experience and knowledge in the real estate
+        industry, our team is devoted to providing top-tier service. We firmly
+        believe that the process of buying or selling a property should be an
+        exhilarating and fulfilling journey, and we are devoted to transforming
+        that belief into reality for each of our valued clients.
+      </p>
+    </div>
+  );
+}
