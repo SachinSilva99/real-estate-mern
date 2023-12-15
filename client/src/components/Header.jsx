cd35fe98dc3a85b568f76f920832736ee813b4c0
SachinSilva99 <sachinmanathunga@gmail.com>
2023-12-15 15:47:33 +0530

    update header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,27 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 const Header = () => {
-
-
   let currentUser = useSelector((state) => state.user.currentUser);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   return (
     <header className="bg-slate-300">
@@ -13,13 +30,20 @@ const Header = () => {
           <span className="text-slate-500">Real</span>
           <span className="text-slate-600">Estate</span>
         </h1>
-        <form className="bg-slate-100 p-3 rounded-md flex items-center justify-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-md flex items-center justify-center"
+        >
           <input
             className="bg-transparent focus:outline-none w-24 sm:w-64"
             type="text"
             placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-400" />
+          <button>
+            <FaSearch className="text-slate-400" />
+          </button>
         </form>
         <ul className="flex items-center gap-4 text-slate-700">
           <Link to="/">
