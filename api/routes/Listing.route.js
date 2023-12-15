de7afcbce93fc1e84e01c701381be2ba5c692e0f
SachinSilva99 <sachinmanathunga@gmail.com>
2023-12-15 22:26:16 +0530

    added get listing public

--- api/routes/Listing.route.js
+++ api/routes/Listing.route.js
@@ -4,14 +4,14 @@ import {
   deleteListing,
   updateListing,
   getListing,
-  getListings
+  getListings,
 } from "../controllers/Listing.controller.js";
 import { verifyToken } from "../utils/VerifyUser.js";
 
 const router = express.Router();
 router.post("", verifyToken, createListing);
 router.delete("/:id", verifyToken, deleteListing);
-router.get("/:id", verifyToken, getListing);
+router.get("/:id", getListing);
 router.get("", getListings);
 router.put("/:id", verifyToken, updateListing);
 export default router;
